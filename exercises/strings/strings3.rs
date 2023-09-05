461c9374d7de6ee096f2c87524cb45749e995fb6
Vinnie Mazza <vinnie.mazza@gmail.com>
2023-09-05 13:16:17 +0000

    42/96

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut trimmed = String::from(input);
+    return trimmed.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut new = String::from(input);
+    new.push_str(" world!");
+    return new
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let new = String::from(input);
+    return new.replace("cars","balloons");
 }
 
 #[cfg(test)]
